f98f2e59ad5997a1f13b661a4551028591af7f4a
Lưu Tiến Phát <luutienphat@gmail.com>
2022-04-29 23:53:39 +0700

    Add 2 new tables to datasource

---
+++ Project Backup Restore/DS.cs
@@ -0,0 +1,25 @@
+﻿namespace Backup_Restore
+{
+}
+namespace Backup_Restore
+{
+
+
+    partial class DS
+    {
+        partial class keysDataTable
+        {
+        }
+
+        partial class SP_STT_BACKUPDataTable
+        {
+        }
+    }
+}
+
+namespace Backup_Restore.DSTableAdapters {
+    
+    
+    public partial class keys {
+    }
+}
